735ab3d96157341400790a6a90974a98296549ea
SmallCad <160338891+SmallCad@users.noreply.github.com>
2024-03-16 04:19:15 -0400

    saving my set up. still figuring out variables and needed recursions

---
+++ PyBank/main.py
@@ -0,0 +1,55 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+
+
+
+# Open and Read in the CSV file
+with open(budget_csv, 'r') as budgetcsv:
+
+    # Spliting the data using ',' as columns 
+    csvreader = csv.reader(budgetcsv, delimiter=',')
+    
+    # Read the header row first and storing it as a list
+    header = next(csvreader)
+
+    #initializing variables to hold items such as total profit, average change
+    #   greatest increase in profits, and greatest decrease in profits
+
+    totalp = 0
+    avchange = 0
+    lst_of_profit = []
+    lst_of_changes = []
+    GIP = 0
+    GDP = 0
+
+    # looping through CSV file 
+    for row in csvreader:
+        totalp = totalp + int(row[1])
+        lst_of_profit.append(int(row[1]))
+
+    for i in range(0, len(lst_of_profit) - 1):
+         lst_of_changes.append(int(lst_of_profit[i+1]) - int(lst_of_profit[i]))
+    #while i in range(0, len(lst_of_profit)):
+
+        
+     #declaring a variable named changes to record changes between months.
+
+    print(f'{lst_of_changes}')
+
+    
+
+
+       
+
+
+
+
+
+
+#Defining a function that analyzes a financial dataset composed of 2
+#   columns: "Date" and "Profit/Losses"
+
+#def pybanking(budget):
