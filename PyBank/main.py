b7b4f2bb87e5cc40a0f391c9d86ca9f7b72222ad
SmallCad <160338891+SmallCad@users.noreply.github.com>
2024-03-16 05:33:24 -0400

    figured out the solution for pybank, now i have to format the printing

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,16 +22,18 @@ with open(budget_csv, 'r') as budgetcsv:
     avchange = 0
     lst_of_profit = []
     lst_of_changes = []
-    GIP = 0
-    GDP = 0
+    GIP = []
+    GDP = []
 
-    # looping through CSV file 
+    ## looping through CSV file 
+
+
     for row in csvreader:
         totalp = totalp + int(row[1])
-        lst_of_profit.append(int(row[1]))
+        lst_of_profit.append(row)
 
     for i in range(0, len(lst_of_profit) - 1):
-         lst_of_changes.append(int(lst_of_profit[i+1]) - int(lst_of_profit[i]))
+         lst_of_changes.append(int(lst_of_profit[i+1][1]) - int(lst_of_profit[i][1]))
     
    
     totalchange = 0
@@ -41,13 +43,42 @@ with open(budget_csv, 'r') as budgetcsv:
        totalchange += changes
        avchange = round(totalchange / length, 2)
 
+    
+    
+    #initializing a variable to be looped through the list of changes in profit
+       #the variables will store the greatest change in profit and losses
+    GIPamount = 0
+    GDPamount = 0
+    
+    
+    #initializing a variable to be looped through the list of changes in profit
+       #the variable will track when the greatest change in profit occurs
+    GIPtracker = 0
+    GDPtracker = 0
+
+    for i in range(0,length):
+        if GIPamount < int(lst_of_changes[i]):
+            GIPtracker = i
+            GIPamount = int(lst_of_changes[i])
         
-        
+        elif GDPamount > int(lst_of_changes[i]):
+            GDPtracker = i
+            GDPamount = int(lst_of_changes[i])
+    
+    GIP.append(lst_of_profit[GIPtracker + 1][0])
+    GIP.append(GIPamount)
+
+    GDP.append(lst_of_profit[GDPtracker + 1][0])
+    GDP.append(GDPamount)
+
+
+
+
 
         
      #declaring a variable named changes to record changes between months.
 
-    print(f'{avchange}')
+    print(*GDP, sep=", ")
 
     
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-challenge
  In this assignment, I'll use the concepts learnt to complete two Python challenges, PyBank and PyPoll.
+
+
+## Credits
+thanks to users Jianru Shi and Steve Bennett for answering and providing the edit to the print code
+ https://stackoverflow.com/questions/11178061/print-list-without-brackets-in-a-single-row
